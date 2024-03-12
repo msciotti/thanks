facf6b8faae423c7a7d3558dcd733ea0ea67a974
Mason Sciotti <mason.sciotti@gmail.com>
2024-03-12 14:22:42 -0400

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,144 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { InteractionResponseFlags, InteractionResponseType, InteractionType, verifyKey } from 'discord-interactions';
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+	prod_thanks: KVNamespace;
 }
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
-};
+	async fetch(request: Request, env: Env, ctx: any): Promise<Response> {
+		const signature = request.headers.get('X-Signature-Ed25519');
+		const timestamp = request.headers.get('X-Signature-Timestamp');
+		const rawBody = await request.clone().text();
+		//@ts-ignore
+		const isValidRequest = verifyKey(rawBody, signature, timestamp, env.DISCORD_PUBLIC_KEY);
+
+		if (!isValidRequest) {
+			return new Response('Invalid signture', { status: 401 });
+		}
+
+		const json: any = await request.json();
+
+		if (json.type === InteractionType.PING) {
+			return new Response(JSON.stringify({
+				type: InteractionResponseType.PONG
+			}));
+		}
+
+		const response = await handleResponse(json, env);
+
+		return new Response(response, {
+			headers: {
+				'Content-type': 'application/json'
+			}
+		});
+	}
+
+}
+
+
+export async function handleResponse(json: any, env: Env): Promise<string> {
+	if (json.type === InteractionType.APPLICATION_COMMAND) {
+		if (json.data.name === 'Thank' || json.data.name === 'thank') {
+			return await handleThankUser(json, env);
+		}
+
+		if (json.data.name === 'my_thanks') {
+			return await handleCheckMyThanks(json, env);
+		}
+	}
+	// Didn't catch a command, something went wrong
+	return JSON.stringify({
+		type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+		data: {
+			flags: InteractionResponseFlags.EPHEMERAL,
+			content: 'Something went wrong. Please try again.'
+		}
+	})
+}
+
+export async function handleThankUser(json: any, env: Env): Promise<string> {
+	// Invoking user is retrieved from a different field depending on
+	// if the command was used in a DM or a server
+	const invokingUserId = json.member?.user.id || json.user.id;
+
+	// The target user can come from one of three sources
+	// 1. A user option of a slash command
+	// 2. The target of a user context menu command
+	// 3. The author of a message context menu command
+	let thankedUserId;
+	switch (json.data.type) {
+		case 1:
+			thankedUserId = json.data.options[0]['value'];
+			break;
+		case 2:
+			thankedUserId = json.data.target_id;
+			break;
+		case 3:
+			thankedUserId = (Object.values(json.data.resolved.messages) as any[])[0].author.id;
+			break;
+		default:
+	}
+
+	if (!thankedUserId || !invokingUserId) {
+		// Couldn't fetch the two users
+		return JSON.stringify({
+			type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+			data: {
+				flags: InteractionResponseFlags.EPHEMERAL,
+				content: 'Something went wrong. Please try again.'
+			}
+		});
+	}
+
+	if (thankedUserId === invokingUserId) {
+		// Can't thank yourself
+		return JSON.stringify({
+			type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+			data: {
+				flags: InteractionResponseFlags.EPHEMERAL,
+				content: 'You can\'t thank yourself!'
+			}
+		});
+	}
+
+	await incrementThankedUser(env, thankedUserId);
+
+	return JSON.stringify({
+		type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+		data: {
+			content: `<@${thankedUserId}> -- you received thanks from <@${invokingUserId}>!`
+		}
+	});
+}
+
+export async function handleCheckMyThanks(json: any, env: Env): Promise<string> {
+	// Invoking user is retrieved from a different field depending on
+	// if the command was used in a DM or a server
+	const invokingUserId = json.member?.user.id || json.user.id;
+
+	const thanksCount = await checkMyThanks(invokingUserId, env);
+
+	return JSON.stringify({
+		type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+		data: {
+			content: thanksCount === 0 ? 
+						`You don't have any thanks yet!` : 
+						`You've received ${thanksCount} thanks :tada:`
+		}
+	});
+}
+
+export async function checkMyThanks(userId: string, env: Env): Promise<number> {
+	const response = await env.prod_thanks.get(userId);
+	// convert to number
+	const myThanks = response ? parseInt(response) : 0;
+	return myThanks;
+}
+
+export async function incrementThankedUser(env: Env, userId: string): Promise<void> {
+	let response = await env.prod_thanks.get(userId);
+	// convert to number
+	const currentThanks = response ? parseInt(response) : 0;
+	const newValue = currentThanks + 1;
+	await env.prod_thanks.put(userId, newValue.toString());
+}
